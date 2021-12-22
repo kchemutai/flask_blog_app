3e3c910efa88629a96717ccf10e6c0bc96a1d831
Chemutai Kevin <kchemutai@gmail.com>
2021-12-22 18:03:10 +0300

    Fixed Edit form not displaying current data on get requests

--- app/routes.py
+++ app/routes.py
@@ -100,15 +100,13 @@ def user(username):
 @login_required
 def edit_profile():
     form = EditProfileForm()
-    if request.method == 'get':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
-
-    elif form.validate_on_submit():
+    if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your profile has been updated successfully!')
+        flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
-    
-    return render_template('edit_profile.html', title='Edit Profile', form = form)
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
